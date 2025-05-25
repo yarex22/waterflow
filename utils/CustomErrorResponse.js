ec1cb2cc35f0f3d04a2413a280341d9f8a0b09ea
Seu Nome no GitHub <seu-email@exemplo.com>
2025-05-25 12:21:38 +0200

    feat: Adicionando classe CustomErrorResponse

---
+++ utils/CustomErrorResponse.js
@@ -0,0 +1,13 @@
+class ErrorResponse extends Error {
+    constructor(message, statusCode, code) {
+        super(message);
+        this.statusCode = statusCode;
+        this.code = code;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = ErrorResponse; 
